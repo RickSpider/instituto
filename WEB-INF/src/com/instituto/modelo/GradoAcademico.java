a535a7ced2fd213188ae3e7f4f0d48629577aeb7
RickSpider <rrgi89@hotmail.com>
2022-04-28 23:29:53 -0400

    Grado Academico

---
+++ WEB-INF/src/com/instituto/modelo/GradoAcademico.java
@@ -0,0 +1,68 @@
+package com.instituto.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.doxacore.modelo.Modelo;
+
+@Entity
+@Table(name ="gradosacademicos")
+public class GradoAcademico extends Modelo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8052259194963768465L;
+
+	@Id
+	@Column(name ="gradoacademicoid")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long gradoacademicoid;
+	private String gradoacademico;
+	private String descripcion;
+	
+	@Override
+	public Object[] getArrayObjectDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStringDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Long getGradoacademicoid() {
+		return gradoacademicoid;
+	}
+
+	public void setGradoacademicoid(Long gradoacademicoid) {
+		this.gradoacademicoid = gradoacademicoid;
+	}
+
+	public String getGradoacademico() {
+		return gradoacademico;
+	}
+
+	public void setGradoacademico(String gradoacademico) {
+		this.gradoacademico = gradoacademico;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+
+}
---
+++ WEB-INF/src/com/instituto/sistema/abm/GradoAcademicoVM.java
@@ -0,0 +1,13 @@
+package com.instituto.sistema.abm;
+
+import com.doxacore.TemplateViewModel;
+
+public class GradoAcademicoVM extends TemplateViewModel {
+
+	@Override
+	protected void inicializarOperaciones() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
