bec4446b40b606b87553189ec07fcbcd9adaea8b
RickSpider <rrgi89@hotmail.com>
2022-05-03 20:32:05 -0400

    correccion de where

--- WEB-INF/sql/buscarMateriaNotCurso.sql
+++ WEB-INF/sql/buscarMateriaNotCurso.sql
@@ -1,8 +1,7 @@
 select 
 m.materiaid, 
 m.materia, 
-m.descripcion, 
-cm.materiaid as cmmateriaid
+m.descripcion
 from materias m
-left join cursosmaterias cm on cm.materiaid = m.materiaid
-where cm.cursoid != ?1 or cm.materiaid is null;;
+where 
+m.materiaid not in (select materiaid from cursosmaterias where cursoid = ?1);
