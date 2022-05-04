9e150afdcfff5aa437c1e1bfe80317ccc92dbc5f
RickSpider <rrgi89@hotmail.com>
2022-05-03 22:12:23 -0400

    agregado metodo currentSede

--- WEB-INF/src/com/instituto/util/TemplateViewModelLocal.java
+++ WEB-INF/src/com/instituto/util/TemplateViewModelLocal.java
@@ -1,9 +1,33 @@
 package com.instituto.util;
 
+import java.util.List;
+
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.Init;
+
 import com.doxacore.TemplateViewModel;
+import com.instituto.modelo.Sede;
+import com.instituto.modelo.UsuarioSede;
 
 public abstract class TemplateViewModelLocal extends TemplateViewModel{
 	
+	@Init(superclass = true)
+	public void initTemplateViewModelLocal() {
+		
+
+	}
+
+	@AfterCompose(superclass = true)
+	public void afterComposeTemplateViewModelLocal() {
+		
+	}
 	
+	protected Sede getCurrentSede() {
+		
+		List<UsuarioSede> result = this.reg.getAllObjectsByCondicionOrder(UsuarioSede.class.getName(), 
+				"usuarioid = "+this.getCurrentUser().getUsuarioid()+" AND activo = true", null);
+		
+		return result.get(0).getSede();
+	}
 
 }
