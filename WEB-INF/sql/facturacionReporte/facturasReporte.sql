38510b8d5948e3acaf5e59adc19c126f660587f4
RickSpider <rrgi89@hotmail.com>
2023-06-12 02:27:45 -0400

    correccion de formato de fecha

--- WEB-INF/sql/facturacionReporte/facturasReporte.sql
+++ WEB-INF/sql/facturacionReporte/facturasReporte.sql
@@ -1,4 +1,4 @@
-SELECT cobranzaid, c.fecha,t.tipo,c.comprobantenum, totaldetalle, (p.nombre||' '||p.apellido)as alumno, anulado
+SELECT cobranzaid, TO_CHAR(c.fecha, 'DD-MM-YYYY HH24:MI:SS') as fecha,t.tipo,c.comprobantenum, totaldetalle, (p.nombre||' '||p.apellido)as alumno, anulado
 	FROM public.cobranzas c
 	left join personas p on p.personaid = c.alumnoid
 	left join tipos t on t.tipoid = c.comprobantetipoid
