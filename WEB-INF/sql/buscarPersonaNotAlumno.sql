81199339f5334f13607fdabdd054d9d22100d025
RickSpider <rrgi89@hotmail.com>
2024-02-28 23:38:01 -0300

    correccion de sql

--- WEB-INF/sql/buscarPersonaNotAlumno.sql
+++ WEB-INF/sql/buscarPersonaNotAlumno.sql
@@ -1,10 +1,9 @@
-SELECT 
+Select 
 p.personaid, 
 concat(p.apellido,', ',p.nombre) as fullname, 
-p.documentonum
+p.documentonum,
+a.alumnoid
 from personas p
-left join alumnos a on a.personaid = p.personaid and a.sedeid != ##SEDEID##
-LEFT JOIN alumnos a_current ON a_current.personaid = p.personaid AND a_current.sedeid = ##SEDEID##
-where a.alumnoid is not null 
-and a_current.personaid IS NULL
-order by p.personaid;
+left join alumnos a on a.personaid = p.personaid and a.sedeid = ##SEDEID##
+where a.alumnoid is null
+order by p.personaid asc;
